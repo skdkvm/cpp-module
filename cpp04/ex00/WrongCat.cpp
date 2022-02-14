70a65aac616a79949efda2e2599609e22d3fffaf
seonggki <tjdrlf0618@gmail.com>
2022-02-14 00:42:26 +0000

    cpp04

--- cpp04/ex00/WrongCat.cpp
+++ cpp04/ex00/WrongCat.cpp
@@ -1,14 +1,14 @@
 #include "WrongCat.hpp"
 
-WrongCat::WrongCat
+WrongCat::WrongCat()
 {
-	std::cout << Green << "WrongCat default constructor called" << Reset <<std::endl;
+	std::cout << Green << "WrongCat default constructor called" << Reset << std::endl;
 	type = "WrongCat";
 }
 
 WrongCat::WrongCat(const WrongCat &ref)
 {
-	std::cout << Green << "WrongCat Copy constructor called" << Reset <<std::endl;
+	std::cout << Green << "WrongCat Copy constructor called" << Reset << std::endl;
 	return *this;
 }
 
@@ -18,7 +18,12 @@ WrongCat& WrongCat::operator=(const WrongCat &ref)
 	return *this;
 }
 
-WrongCat::~WrongCat
+WrongCat::~WrongCat()
 {
 	std::cout << Red << "WrongCat default constructor called" << Reset << std::endl;
+}
+
+void	WrongCat::makeSound() const
+{
+	std::cout << "Wrong Meow Wrong Meow" << std::endl;	
 }
