51cb51fcf10dfa6bb1a7bb7281a259e34c295636
Seonggil Kim <seonggki@c6r7s2.42seoul.kr>
2022-03-04 19:04:21 +0900

    cpp08

--- cpp08/ex02/main.cpp
+++ cpp08/ex02/main.cpp
@@ -4,66 +4,67 @@
 
 int main()
 {
+	std::cout << "===== MutantStack test =====" <<  std::endl;
 	MutantStack<int> mstack;
 	mstack.push(5);
 	mstack.push(17);
-	std::cout << mstack.top() << std::endl;
+	std::cout << Green << mstack.top() << Reset << std::endl;
 	mstack.pop();
-	std::cout << mstack.size() << std::endl;
+	std::cout << Green << mstack.size() << Reset << std::endl;
 	mstack.push(3);
 	mstack.push(5);
 	mstack.push(737);
 	mstack.push(0);
 	MutantStack<int>::iterator it = mstack.begin();
 	MutantStack<int>::iterator ite = mstack.end();
-	std::cout << "mstack begin : " << *it << std::endl;
-	std::cout << "mstack end : "<< *(ite - 1) << std::endl;
+	std::cout << "mstack begin : " << Green << *it << Reset << std::endl;
+	std::cout << "mstack end : "<< Green << *(ite - 1) << Reset << std::endl;
 	++it;
-	std::cout << "mstack begin + 1 : " << *it << std::endl;
+	std::cout << "mstack begin + 1 : " << Green << *it << Reset << std::endl;
 	--it;
 	std::cout << "mstack list : ";
 	while (it != ite)
 	{
-		std::cout << *it << " ";
+		std::cout << Green << *it << " ";
 		++it;
 	}
-	std::cout << std::endl;
+	std::cout << Reset << std::endl;
 
-	std::cout << "List test" << std::endl;
+	std::cout << "===== List test =====" << std::endl;
 	std::list<int>	ltest;
 	ltest.push_back(5);
 	ltest.push_back(17);
-	std::cout << ltest.back() << std::endl;
+	std::cout << Yellow << ltest.back() << Reset << std::endl;
 	ltest.pop_back();
-	std::cout << ltest.size() << std::endl;
+	std::cout << Yellow << ltest.size() << Reset << std::endl;
 	ltest.push_back(3);
 	ltest.push_back(5);
 	ltest.push_back(737);
 	ltest.push_back(0);
 	std::list<int>::iterator lit = ltest.begin();
 	std::list<int>::iterator lite = ltest.end();
-	std::cout << "ltest begin : " << *lit << std::endl;
-	std::cout << "ltest end : "<< ltest.back() << std::endl;
+	std::cout << "ltest begin : " << Yellow << *lit << Reset << std::endl;
+	std::cout << "ltest end : " << Yellow << ltest.back() << Reset<< std::endl;
 	++lit;
-	std::cout << "ltest begin + 1 : " << *lit << std::endl;
+	std::cout << "ltest begin + 1 : " << Yellow << *lit << Reset << std::endl;
 	--lit;
 	std::cout << "ltest list : ";
 	while (lit != lite)
 	{
-		std::cout << *lit << " ";
+		std::cout << Yellow << *lit << " ";
 		++lit;
 	}
-	std::cout << std::endl;
+	std::cout << Reset << std::endl;
 	
 	std::stack<int> s(mstack);
 	mstack.pop();
 	mstack.push(42);
 	mstack.push(77);
-	std::cout << "copy check " << std::endl;
-	std::cout << "size : " << s.size() << std::endl;
-	std::cout << "top : " << s.top() << std::endl;
-	std::cout << "mstack check " << std::endl;
-	std::cout << "size : " << mstack.size() << std::endl;
-	std::cout << "top : " << mstack.top() << std::endl;
+	std::cout << Green << "copy check " << Reset << std::endl;
+	std::cout << Green << "size : " << Reset <<  s.size() << std::endl;
+	std::cout << Green << "top : " << Reset << s.top() << std::endl;
+	std::cout << Green << "mstack check " << Reset << std::endl;
+	std::cout << Green << "size : " << Reset << mstack.size() << std::endl;
+	std::cout << Green << "top : " << Reset << mstack.top() << std::endl;
 	return 0;
 }
